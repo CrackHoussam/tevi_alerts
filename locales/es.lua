e04a62f4cfc87cba5f7ac140ca0a47ca4a9db58a
CrackHoussam <75907639+CrackHoussam@users.noreply.github.com>
2020-12-13 20:52:12 +0100

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -1,6 +1,6 @@
 Locales['es'] = {
-['msg1'] = "Niveles de alertas",
-['msg2'] = "Nivel de alerta 0",
-['msg3'] = "Nivel de alerta 1",
-['msg4'] = "Nivel de alerta 2",
+['msg1'] = "Niveau d'alerte",
+['msg2'] = "Niveau d'alerte 0",
+['msg3'] = "Niveau d'alerte 1",
+['msg4'] = "Niveau d'alerte 2",
 }
